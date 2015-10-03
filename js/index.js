81818e29c81163c719565cee82f10ec1532ec655
wra216 <wellesleyarreza@ymail.com>
2015-10-03 15:42:43 -0400

    updated with pins

--- js/index.js
+++ js/index.js
@@ -1,11 +1,70 @@
-function initialize() {
-  var mapCanvas = document.getElementById('map');
-  var mapOptions = {
-      center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+$(function(){
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function initMap() {
+                var uluru = {lat: -25.363, lng: 131.044};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    center: uluru
+                });
+
+                var contentString = '<div id="content">' +
+                        '<div id="siteNotice">' +
+                        '</div>' +
+                        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+                        '<div id="bodyContent">' +
+                        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                        'sandstone rock formation in the southern part of the ' +
+                        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+                        'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+                        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+                        'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+                        'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+                        'Aboriginal people of the area. It has many springs, waterholes, ' +
+                        'rock caves and ancient paintings. Uluru is listed as a World ' +
+                        'Heritage Site.</p>' +
+                        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+                        'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+                        '(last visited June 22, 2009).</p>' +
+                        '</div>' +
+                        '</div>';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map,
+                    title: 'Uluru (Ayers Rock)'
+                });
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
+                
+                var marker2 = new google.maps.Marker({
+                    position: {lat: -22.363, lng: 129.044},
+                    map: map,
+                    title: 'P2'
+                });
+                var marker3 = new google.maps.Marker({
+                    position: {lat: -20.363, lng: 121.044},
+                    map: map,
+                    title: 'P3'
+                });
+                var marker4 = new google.maps.Marker({
+                    position: {lat: -25.363, lng: 14.044},
+                    map: map,
+                    title: 'P4'
+                });
+                var marker5 = new google.maps.Marker({
+                    position: {lat: -28.363, lng: 113.044},
+                    map: map,
+                    title: 'P5'
+                });
+                
+                
+            }
+
+
+});
+
