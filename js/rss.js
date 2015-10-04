db4eb5912422c0faf787d218c8d4cb554b8ce9b7
wra216 <wellesleyarreza@ymail.com>
2015-10-04 06:55:15 -0400

    change

--- js/rss.js
+++ js/rss.js
@@ -1,8 +1,255 @@
-<<<<<<< HEAD
-			var urlSTR;
-=======
+var jsonArr=[ 
+{"name": "Afghanistan", "code": "AF"}, 
+{"name": "Åland Islands", "code": "AX"}, 
+{"name": "Albania", "code": "AL"}, 
+{"name": "Algeria", "code": "DZ"}, 
+{"name": "American Samoa", "code": "AS"}, 
+{"name": "AndorrA", "code": "AD"}, 
+{"name": "Angola", "code": "AO"}, 
+{"name": "Anguilla", "code": "AI"}, 
+{"name": "Antarctica", "code": "AQ"}, 
+{"name": "Antigua and Barbuda", "code": "AG"}, 
+{"name": "Argentina", "code": "AR"}, 
+{"name": "Armenia", "code": "AM"}, 
+{"name": "Aruba", "code": "AW"}, 
+{"name": "Australia", "code": "AU"}, 
+{"name": "Austria", "code": "AT"}, 
+{"name": "Azerbaijan", "code": "AZ"}, 
+{"name": "Bahamas", "code": "BS"}, 
+{"name": "Bahrain", "code": "BH"}, 
+{"name": "Bangladesh", "code": "BD"}, 
+{"name": "Barbados", "code": "BB"}, 
+{"name": "Belarus", "code": "BY"}, 
+{"name": "Belgium", "code": "BE"}, 
+{"name": "Belize", "code": "BZ"}, 
+{"name": "Benin", "code": "BJ"}, 
+{"name": "Bermuda", "code": "BM"}, 
+{"name": "Bhutan", "code": "BT"}, 
+{"name": "Bolivia", "code": "BO"}, 
+{"name": "Bosnia and Herzegovina", "code": "BA"}, 
+{"name": "Botswana", "code": "BW"}, 
+{"name": "Bouvet Island", "code": "BV"}, 
+{"name": "Brazil", "code": "BR"}, 
+{"name": "British Indian Ocean Territory", "code": "IO"}, 
+{"name": "Brunei Darussalam", "code": "BN"}, 
+{"name": "Bulgaria", "code": "BG"}, 
+{"name": "Burkina Faso", "code": "BF"}, 
+{"name": "Burundi", "code": "BI"}, 
+{"name": "Cambodia", "code": "KH"}, 
+{"name": "Cameroon", "code": "CM"}, 
+{"name": "Canada", "code": "CA"}, 
+{"name": "Cape Verde", "code": "CV"}, 
+{"name": "Cayman Islands", "code": "KY"}, 
+{"name": "Central African Republic", "code": "CF"}, 
+{"name": "Chad", "code": "TD"}, 
+{"name": "Chile", "code": "CL"}, 
+{"name": "China", "code": "CN"}, 
+{"name": "Christmas Island", "code": "CX"}, 
+{"name": "Cocos (Keeling) Islands", "code": "CC"}, 
+{"name": "Colombia", "code": "CO"}, 
+{"name": "Comoros", "code": "KM"}, 
+{"name": "Congo", "code": "CG"}, 
+{"name": "Congo, The Democratic Republic of the", "code": "CD"}, 
+{"name": "Cook Islands", "code": "CK"}, 
+{"name": "Costa Rica", "code": "CR"}, 
+{"name": "Cote D\"Ivoire", "code": "CI"}, 
+{"name": "Croatia", "code": "HR"}, 
+{"name": "Cuba", "code": "CU"}, 
+{"name": "Cyprus", "code": "CY"}, 
+{"name": "Czech Republic", "code": "CZ"}, 
+{"name": "Denmark", "code": "DK"}, 
+{"name": "Djibouti", "code": "DJ"}, 
+{"name": "Dominica", "code": "DM"}, 
+{"name": "Dominican Republic", "code": "DO"}, 
+{"name": "Ecuador", "code": "EC"}, 
+{"name": "Egypt", "code": "EG"}, 
+{"name": "El Salvador", "code": "SV"}, 
+{"name": "Equatorial Guinea", "code": "GQ"}, 
+{"name": "Eritrea", "code": "ER"}, 
+{"name": "Estonia", "code": "EE"}, 
+{"name": "Ethiopia", "code": "ET"}, 
+{"name": "Falkland Islands (Malvinas)", "code": "FK"}, 
+{"name": "Faroe Islands", "code": "FO"}, 
+{"name": "Fiji", "code": "FJ"}, 
+{"name": "Finland", "code": "FI"}, 
+{"name": "France", "code": "FR"}, 
+{"name": "French Guiana", "code": "GF"}, 
+{"name": "French Polynesia", "code": "PF"}, 
+{"name": "French Southern Territories", "code": "TF"}, 
+{"name": "Gabon", "code": "GA"}, 
+{"name": "Gambia", "code": "GM"}, 
+{"name": "Georgia", "code": "GE"}, 
+{"name": "Germany", "code": "DE"}, 
+{"name": "Ghana", "code": "GH"}, 
+{"name": "Gibraltar", "code": "GI"}, 
+{"name": "Greece", "code": "GR"}, 
+{"name": "Greenland", "code": "GL"}, 
+{"name": "Grenada", "code": "GD"}, 
+{"name": "Guadeloupe", "code": "GP"}, 
+{"name": "Guam", "code": "GU"}, 
+{"name": "Guatemala", "code": "GT"}, 
+{"name": "Guernsey", "code": "GG"}, 
+{"name": "Guinea", "code": "GN"}, 
+{"name": "Guinea-Bissau", "code": "GW"}, 
+{"name": "Guyana", "code": "GY"}, 
+{"name": "Haiti", "code": "HT"}, 
+{"name": "Heard Island and Mcdonald Islands", "code": "HM"}, 
+{"name": "Holy See (Vatican City State)", "code": "VA"}, 
+{"name": "Honduras", "code": "HN"}, 
+{"name": "Hong Kong", "code": "HK"}, 
+{"name": "Hungary", "code": "HU"}, 
+{"name": "Iceland", "code": "IS"}, 
+{"name": "India", "code": "IN"}, 
+{"name": "Indonesia", "code": "ID"}, 
+{"name": "Iran, Islamic Republic Of", "code": "IR"}, 
+{"name": "Iraq", "code": "IQ"}, 
+{"name": "Ireland", "code": "IE"}, 
+{"name": "Isle of Man", "code": "IM"}, 
+{"name": "Israel", "code": "IL"}, 
+{"name": "Italy", "code": "IT"}, 
+{"name": "Jamaica", "code": "JM"}, 
+{"name": "Japan", "code": "JP"}, 
+{"name": "Jersey", "code": "JE"}, 
+{"name": "Jordan", "code": "JO"}, 
+{"name": "Kazakhstan", "code": "KZ"}, 
+{"name": "Kenya", "code": "KE"}, 
+{"name": "Kiribati", "code": "KI"}, 
+{"name": "Korea, Democratic People\"S Republic of", "code": "KP"}, 
+{"name": "Korea, Republic of", "code": "KR"}, 
+{"name": "Kuwait", "code": "KW"}, 
+{"name": "Kyrgyzstan", "code": "KG"}, 
+{"name": "Lao People\"S Democratic Republic", "code": "LA"}, 
+{"name": "Latvia", "code": "LV"}, 
+{"name": "Lebanon", "code": "LB"}, 
+{"name": "Lesotho", "code": "LS"}, 
+{"name": "Liberia", "code": "LR"}, 
+{"name": "Libyan Arab Jamahiriya", "code": "LY"}, 
+{"name": "Liechtenstein", "code": "LI"}, 
+{"name": "Lithuania", "code": "LT"}, 
+{"name": "Luxembourg", "code": "LU"}, 
+{"name": "Macao", "code": "MO"}, 
+{"name": "Macedonia, The Former Yugoslav Republic of", "code": "MK"}, 
+{"name": "Madagascar", "code": "MG"}, 
+{"name": "Malawi", "code": "MW"}, 
+{"name": "Malaysia", "code": "MY"}, 
+{"name": "Maldives", "code": "MV"}, 
+{"name": "Mali", "code": "ML"}, 
+{"name": "Malta", "code": "MT"}, 
+{"name": "Marshall Islands", "code": "MH"}, 
+{"name": "Martinique", "code": "MQ"}, 
+{"name": "Mauritania", "code": "MR"}, 
+{"name": "Mauritius", "code": "MU"}, 
+{"name": "Mayotte", "code": "YT"}, 
+{"name": "Mexico", "code": "MX"}, 
+{"name": "Micronesia, Federated States of", "code": "FM"}, 
+{"name": "Moldova, Republic of", "code": "MD"}, 
+{"name": "Monaco", "code": "MC"}, 
+{"name": "Mongolia", "code": "MN"}, 
+{"name": "Montserrat", "code": "MS"}, 
+{"name": "Morocco", "code": "MA"}, 
+{"name": "Mozambique", "code": "MZ"}, 
+{"name": "Myanmar", "code": "MM"}, 
+{"name": "Namibia", "code": "NA"}, 
+{"name": "Nauru", "code": "NR"}, 
+{"name": "Nepal", "code": "NP"}, 
+{"name": "Netherlands", "code": "NL"}, 
+{"name": "Netherlands Antilles", "code": "AN"}, 
+{"name": "New Caledonia", "code": "NC"}, 
+{"name": "New Zealand", "code": "NZ"}, 
+{"name": "Nicaragua", "code": "NI"}, 
+{"name": "Niger", "code": "NE"}, 
+{"name": "Nigeria", "code": "NG"}, 
+{"name": "Niue", "code": "NU"}, 
+{"name": "Norfolk Island", "code": "NF"}, 
+{"name": "Northern Mariana Islands", "code": "MP"}, 
+{"name": "Norway", "code": "NO"}, 
+{"name": "Oman", "code": "OM"}, 
+{"name": "Pakistan", "code": "PK"}, 
+{"name": "Palau", "code": "PW"}, 
+{"name": "Palestinian Territory, Occupied", "code": "PS"}, 
+{"name": "Panama", "code": "PA"}, 
+{"name": "Papua New Guinea", "code": "PG"}, 
+{"name": "Paraguay", "code": "PY"}, 
+{"name": "Peru", "code": "PE"}, 
+{"name": "Philippines", "code": "PH"}, 
+{"name": "Pitcairn", "code": "PN"}, 
+{"name": "Poland", "code": "PL"}, 
+{"name": "Portugal", "code": "PT"}, 
+{"name": "Puerto Rico", "code": "PR"}, 
+{"name": "Qatar", "code": "QA"}, 
+{"name": "Reunion", "code": "RE"}, 
+{"name": "Romania", "code": "RO"}, 
+{"name": "Russian Federation", "code": "RU"}, 
+{"name": "RWANDA", "code": "RW"}, 
+{"name": "Saint Helena", "code": "SH"}, 
+{"name": "Saint Kitts and Nevis", "code": "KN"}, 
+{"name": "Saint Lucia", "code": "LC"}, 
+{"name": "Saint Pierre and Miquelon", "code": "PM"}, 
+{"name": "Saint Vincent and the Grenadines", "code": "VC"}, 
+{"name": "Samoa", "code": "WS"}, 
+{"name": "San Marino", "code": "SM"}, 
+{"name": "Sao Tome and Principe", "code": "ST"}, 
+{"name": "Saudi Arabia", "code": "SA"}, 
+{"name": "Senegal", "code": "SN"}, 
+{"name": "Serbia and Montenegro", "code": "CS"}, 
+{"name": "Seychelles", "code": "SC"}, 
+{"name": "Sierra Leone", "code": "SL"}, 
+{"name": "Singapore", "code": "SG"}, 
+{"name": "Slovakia", "code": "SK"}, 
+{"name": "Slovenia", "code": "SI"}, 
+{"name": "Solomon Islands", "code": "SB"}, 
+{"name": "Somalia", "code": "SO"}, 
+{"name": "South Africa", "code": "ZA"}, 
+{"name": "South Georgia and the South Sandwich Islands", "code": "GS"}, 
+{"name": "Spain", "code": "ES"}, 
+{"name": "Sri Lanka", "code": "LK"}, 
+{"name": "Sudan", "code": "SD"}, 
+{"name": "Suriname", "code": "SR"}, 
+{"name": "Svalbard and Jan Mayen", "code": "SJ"}, 
+{"name": "Swaziland", "code": "SZ"}, 
+{"name": "Sweden", "code": "SE"}, 
+{"name": "Switzerland", "code": "CH"}, 
+{"name": "Syrian Arab Republic", "code": "SY"},
+{"name": "Syria", "code": "SY"}, 
+{"name": "Taiwan, Province of China", "code": "TW"}, 
+{"name": "Tajikistan", "code": "TJ"}, 
+{"name": "Tanzania, United Republic of", "code": "TZ"}, 
+{"name": "Thailand", "code": "TH"}, 
+{"name": "Timor-Leste", "code": "TL"}, 
+{"name": "Togo", "code": "TG"}, 
+{"name": "Tokelau", "code": "TK"}, 
+{"name": "Tonga", "code": "TO"}, 
+{"name": "Trinidad and Tobago", "code": "TT"}, 
+{"name": "Tunisia", "code": "TN"}, 
+{"name": "Turkey", "code": "TR"}, 
+{"name": "Turkmenistan", "code": "TM"}, 
+{"name": "Turks and Caicos Islands", "code": "TC"}, 
+{"name": "Tuvalu", "code": "TV"}, 
+{"name": "Uganda", "code": "UG"}, 
+{"name": "Ukraine", "code": "UA"}, 
+{"name": "United Arab Emirates", "code": "AE"}, 
+{"name": "United Kingdom", "code": "GB"}, 
+{"name": "United States", "code": "US"}, 
+{"name": "United States Minor Outlying Islands", "code": "UM"}, 
+{"name": "Uruguay", "code": "UY"}, 
+{"name": "Uzbekistan", "code": "UZ"}, 
+{"name": "Vanuatu", "code": "VU"}, 
+{"name": "Venezuela", "code": "VE"}, 
+{"name": "Viet Nam", "code": "VN"}, 
+{"name": "Virgin Islands, British", "code": "VG"}, 
+{"name": "Virgin Islands, U.S.", "code": "VI"}, 
+{"name": "Wallis and Futuna", "code": "WF"}, 
+{"name": "Western Sahara", "code": "EH"}, 
+{"name": "Yemen", "code": "YE"}, 
+{"name": "Zambia", "code": "ZM"}, 
+{"name": "Zimbabwe", "code": "ZW"} 
+];
+
+
+		var urlSTR;
+
 	    var urlSTR;
->>>>>>> 3c8c6cc0c70b1a772280c9edb5f4412157f5954e
+
             var temp;
             var map;
             var markers = [];
@@ -13,8 +260,8 @@
                 var uluru = {lat: 25.2916097, lng: -107.2902839};
                 map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 4,
-<<<<<<< HEAD
-                    center: uluru
+ 					center: uluru,
+                    styles: [{"featureType":"all","elementType":"all","stylers":[{"saturation":-100},{"gamma":0.5}]}]
                 });
 
 					// click on search button to search.
@@ -24,9 +271,8 @@
 					
 					deleteMarkers();
 
-=======
-                    center: uluru,
-                    styles: [{"featureType":"all","elementType":"all","stylers":[{"saturation":-100},{"gamma":0.5}]}]
+
+                
                 });
 
 
@@ -36,7 +282,7 @@
 			
 			
 		   deleteMarkers();	
->>>>>>> 3c8c6cc0c70b1a772280c9edb5f4412157f5954e
+
                     var str=$("#search").val();
                     var arr=str.split(" ");
                     var urlParam="";
@@ -57,47 +303,36 @@
                         //http://feeds.reuters.com/Reuters/worldNews
                         success: function (data) {
 
-                            console.log(JSON.stringify(eval('(' + data + ')')));
+                            //console.log(JSON.stringify(eval('(' + data + ')')));
                             data = JSON.stringify(eval('(' + data + ')'));
                             data = JSON.parse(data);
                             $("#feed").empty();
 
                             data.responseData.results.forEach(function (d) {
-<<<<<<< HEAD
 
 
-                                console.log(d);
-=======
-                                //console.log(d);
->>>>>>> 3c8c6cc0c70b1a772280c9edb5f4412157f5954e
-                                //content
-                                //publisher
-                                //title
-                                //url
-                                
-				var randomLat = (Math.floor(Math.random() * 7) + 1) * (Math.floor(Math.random()*2) === 1 ? 1 : -1);
-                                var randomLng = (Math.floor(Math.random() * 7) + 1) * (Math.floor(Math.random()*2) === 1 ? 1 : -1);
-                                var link = d.unescapedUrl;
-                                var title = d.title;
-<<<<<<< HEAD
-                                var randomLat = (Math.floor(Math.random() * 7) + 1) * (Math.floor(Math.random()*2) === 1 ? 1 : -1);
-                                var randomLng = (Math.floor(Math.random() * 7) + 1) * (Math.floor(Math.random()*2) === 1 ? 1 : -1);
-                                var description=d.content;
-
-                                $("#feed").append("<p><a href='" + link + "'>" + title + "</a></br></p>");
-
-                                addMarker( {lat: 43.0759678 + randomLat, lng: -107.2902839 + randomLng}, 'The New York Times', link, title, description);
+                            	//https://maps.googleapis.com/maps/api/geocode/json?address=China&key=AIzaSyDa9qiut_2abe13UhwEIlWJSwqR9nCPTwY
 
 
+                            	// search content for one instance of country
+                            	// get country
+                            	// do ajax call
+                            	//get longitude and latitude
 
-=======
-                                var content = d.content;
-                                var publisher = d.publisher;
-                                $("#feed").append("<div class = 'feed-cell'><a href='"+link+"' target='_blank'><h1>"+title+"</h1><p>"+content+"</p><h3>"+publisher+"   <button type='button' class='myButton' id='link-go' style='color: rgb(193,39,45) !important'><center><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span></center></button></h3></a></div>");
-                            	addMarker( {lat: 43.0759678 + randomLat, lng: -107.2902839 + randomLng}, '', link, title, d.content);
+                            	var country="United States";
+                            	for(i=0; i<Object.keys(jsonArr).length	; i++){
+                            		if((d.content+d.title ).indexOf(jsonArr[i].name)>-1){
+                            			console.log(jsonArr[i].name);
+                            			country=jsonArr[i].name;
+                            			break;
+                            		}
+                            	}
+
+
+                            	var obj=getLocation(country,d)
+          	
                             	
-                            	
->>>>>>> 3c8c6cc0c70b1a772280c9edb5f4412157f5954e
+
                             });
 
                         },
@@ -107,7 +342,53 @@
                     });
                 });
                 
-                function addMarker(location,source, link, title, description) {
+            
+            function getLocation(country , d){
+            	//https://maps.googleapis.com/maps/api/geocode/json?address=China&key=AIzaSyDa9qiut_2abe13UhwEIlWJSwqR9nCPTwY
+            	var longitude;
+            	var latitude;
+            	var urlSTR='https://maps.googleapis.com/maps/api/geocode/json?address='+country+'&key=AIzaSyDa9qiut_2abe13UhwEIlWJSwqR9nCPTwY';
+            	$.ajax({
+                        url: 'http://sleepy-sierra-9008.herokuapp.com/', //http://localhost:8080/
+                        type: "POST",
+                        data: urlSTR,
+                        success: function (data) {
+                        	 data = JSON.parse(data);
+                        	 console.log(data);
+                        	 latitude=data.results[0].geometry.location.lat;
+                        	 longitude=data.results[0].geometry.location.lng;
+                        	 console.log(latitude);
+                        	 var obj=[];
+                        	 obj[0]=latitude;
+                        	 obj[1]=longitude;
+                        	console.log(obj[0]);								
+                                var link = d.unescapedUrl;
+                                var title = d.title;
+
+                                var randomLat = (Math.floor(Math.random() * 7) + 1) * (Math.floor(Math.random()*2) === 1 ? 1 : -1);
+                                var randomLng = (Math.floor(Math.random() * 7) + 1) * (Math.floor(Math.random()*2) === 1 ? 1 : -1);
+                                var description=d.content;
+
+                                var content = d.content;
+                                var publisher = d.publisher;
+                                $("#feed").append("<div class = 'feed-cell'><a href='"+link+"' target='_blank'><h1>"+title+"</h1><p>"+content+"</p><h3>"+publisher+"   <button type='button' class='myButton' id='link-go' style='color: rgb(193,39,45) !important'><center><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span></center></button></h3></a></div>");
+                            	addMarker( {lat: latitude + randomLat, lng: longitude + randomLng}, '', link, title, d.content);
+                  
+
+
+
+                        },
+                        failure: function (err) {
+                            console.log(err);
+                        }
+                    });
+
+            	
+
+            }
+
+
+            function addMarker(location,source, link, title, description) {
   			var marker = new google.maps.Marker({
     				position: location,
   	  				map: map
