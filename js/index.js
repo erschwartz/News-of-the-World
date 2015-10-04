a3e2024eb455c84fb0a924e5a7842c1c2cfb8f5f
Erika Schwartz <erikaschwartz@comcast.net>
2015-10-03 23:07:31 -0400

    UI changes

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
             function initMap() {
                 var uluru = {lat: -25.363, lng: 131.044};
                 var map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
+                    zoom: 2,
                     center: uluru
                 });
 
